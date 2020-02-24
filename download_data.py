6e35503ba596223115b9ed823f7e8aa28525543c
ixgauth <ixgauth@yahoo.com>
2020-02-24 17:12:37 -0500

    working on json error.

--- download_data.py
+++ download_data.py
@@ -2,15 +2,15 @@ import requests
 import json
 import re
 
-baseURL = "https://gerrit-review.googlesource.com/changes/?q=project:gerrit&n=1&o=ALL_FILES&o=LABELS"
+baseURL = "https://gerrit-review.googlesource.com/changes/?q=project:gerrit&n=5&o=ALL_FILES&o=LABELS"
 
 resp = requests.get(baseURL)
 # resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
 
 if(resp.status_code == 200):
-    print(resp.content)
+    print(resp.content.decode("utf-8"))
     print("it worked")
-    print(json.loads(resp.content))
+    print(json.loads(resp.content.decode("utf-8")))
 else:
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 

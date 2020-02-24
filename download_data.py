23f4e167fc4af8849b4f9decdeb50e8e2263760f
ixgauth <ixgauth@yahoo.com>
2020-02-24 18:01:49 -0500

    rest query working better now

--- download_data.py
+++ download_data.py
@@ -2,13 +2,13 @@ import requests
 import json
 import re
 
-baseURL = "https://gerrit-review.googlesource.com/changes/?q=project:gerrit&n=5&o=ALL_FILES&o=LABELS"
+baseURL = "https://gerrit-review.googlesource.com/changes/?q=project:gerrit&S=3000&n=500&o=ALL_FILES&o=LABELS"
 
 resp = requests.get(baseURL)
 # resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
 
 if(resp.status_code == 200):
-    print(resp.content.decode("utf-8"))
+    # print(resp.content.decode("utf-8"))
     print("it worked")
     loaded = json.loads(resp.content.decode("utf-8").replace(")]}'",''))
 else:

ebf018071a96d681726be281fe0067b210490569
ixgauth <ixgauth@yahoo.com>
2020-02-24 18:13:27 -0500

    working on pulling multiple groups of changes

--- download_data.py
+++ download_data.py
@@ -2,17 +2,22 @@ import requests
 import json
 import re
 
-baseURL = "https://gerrit-review.googlesource.com/changes/?q=project:gerrit&S=3000&n=500&o=ALL_FILES&o=LABELS"
+S = 1
+while True:
+	print("S=",S)
+	baseURL = f"https://gerrit-review.googlesource.com/changes/?q=project:gerrit&S={S}&n=500&o=ALL_FILES&o=LABELS"
+	print(baseURL)
+	resp = requests.get(baseURL)
+	# resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
 
-resp = requests.get(baseURL)
-# resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
-
-if(resp.status_code == 200):
-    # print(resp.content.decode("utf-8"))
-    print("it worked")
-    loaded = json.loads(resp.content.decode("utf-8").replace(")]}'",''))
-else:
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+	if(resp.status_code == 200):
+	    print("it worked")
+	    loaded = json.loads(resp.content.decode("utf-8").replace(")]}'",''))
+	    S+=500
+	else:
+	    break
+	if S > 5000:
+		break
 
 with open('data.json','w') as outfile:
 	outfile.write(resp.content.decode("utf-8").replace(")]}'",'').strip())

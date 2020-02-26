7f9531a76011ef5eacb884aacab6f02adb7ea15f
ixgauth <ixgauth@yahoo.com>
2020-02-26 03:15:10 -0500

    think that this should work for loading json

--- get_comments.py
+++ get_comments.py
@@ -12,9 +12,14 @@ def intake_data_frame(filename):
 	df = pd.DataFrame(data["commits"])
 	return df
 
-data = []
-for line in open('test_data.json', 'r'):
-	print(line)
-	data.append(json.loads(line))
+# data = []
+# for line in open('test_data.json', 'r'):
+# 	# print(line)
+# 	data.append(json.loads(line))
 
-print(data.columns)
+# df = pd.DataFrame(data)
+
+with open('test_data.json') as json_file:
+	data = json.load(json_file)
+
+print(data[0]['change_id'])

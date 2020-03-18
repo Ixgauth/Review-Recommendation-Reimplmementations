7f8a489b47589810f255e0b5c3daa5566ac79317
ixgauth <ixgauth@yahoo.com>
2020-03-18 14:16:03 -0400

    think that i'm getting comments working correctly rn.

--- data_extract.py
+++ data_extract.py
@@ -8,9 +8,26 @@ def get_files_for_rev(revision):
 		files.append(filename)
 	return files
 
+def get_all_files_for_commit(commit):
+	all_files = []
+	for key in commit.keys():
+		all_files = all_files + get_files_for_rev(line[key])
+	return all_files
+
+def get_comment_files(commit):
+	files = []
+	for line in commit.keys():
+		files.append(line)
+	return(files)
+
+
 df = pd.read_json('test_data_with_comments.json')
 
 for line in df['revisions']:
-	for key in line.keys():
-		files = get_files_for_rev(line[key])
-		print(files)
+	files = get_all_files_for_commit(line)
+	print(files)
+
+
+for line in df['comments']:
+	print(get_comment_files(line))
+	print(line)

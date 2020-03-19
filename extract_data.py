fb626cd1db22739f1b9e4223bf4032f1e7cc724f
ixgauth <ixgauth@yahoo.com>
2020-03-19 16:53:29 -0400

    now able to get both author and time of commit

--- extract_data.py
+++ extract_data.py
@@ -7,35 +7,35 @@ import requests
 
 
 def get_comment_info(single_comment):
-	authors = []
+	info = []
 	for line in single_comment:
-		authors.append(line['author']['_account_id'])
-	return authors
+		info_tuple = line['author']['_account_id'], line['updated']
+		info.append(info_tuple)
+	return info
+
+# def get_comment_times(comment):
+
+
+def get_comment_tuples(owner, comments):
+	tuple_list = []
+	for key in comments.keys():
+		comment_info = get_comment_info(comments[key])
+
+		for author in comment_info:
+			if owner == author[0]:
+				continue
+			else:
+				file_comment_tuple = (key, author[0],author[1])
+				tuple_list.append(file_comment_tuple)
+	return tuple_list
 
 df = pd.read_json('test_data_with_comments.json')
-
-for line in df['owner']:
-	print(line["_account_id"])
 
 file_comment_tuple_list = []
 
 for i in range(0, len(df['owner'])):
 	comments = df['comments'][i]
 	if isinstance(comments, dict):
-		for key in comments.keys():
-			print(key)
-			authors = get_comment_info(comments[key])
-			for author in authors:
-				if df['owner'][i]["_account_id"] == author:
-					print('author matches')
-				else:
-					file_comment_tuple = (key, author)
-					print(file_comment_tuple)
-					file_comment_tuple_list.append(file_comment_tuple)
-print(len(file_comment_tuple_list))
-
-# for line in df['comments']:
-# 	if isinstance(line,dict):
-# 		for key in line.keys():
-# 			get_comment_info(line[key])
+		file_comment_tuple_list = file_comment_tuple_list + get_comment_tuples(df['owner'][i]["_account_id"], comments)
 			
+print(len(file_comment_tuple_list))

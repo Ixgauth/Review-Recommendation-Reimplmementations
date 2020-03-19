bf252d0179bd79058ac4c2db24c420cee30d465d
ixgauth <ixgauth@yahoo.com>
2020-03-18 21:22:34 -0400

    getting tuples for all author connected with their comments.

--- extract_data.py
+++ extract_data.py
@@ -7,15 +7,35 @@ import requests
 
 
 def get_comment_info(single_comment):
+	authors = []
 	for line in single_comment:
-		if 'line' in line.keys():
-			print(line['line'])
-		else:
-			print('nope')
+		authors.append(line['author']['_account_id'])
+	return authors
 
 df = pd.read_json('test_data_with_comments.json')
 
-for line in df['comments']:
-	if isinstance(line,dict):
-		for key in line.keys():
-			get_comment_info(line[key])
+for line in df['owner']:
+	print(line["_account_id"])
+
+file_comment_tuple_list = []
+
+for i in range(0, len(df['owner'])):
+	comments = df['comments'][i]
+	if isinstance(comments, dict):
+		for key in comments.keys():
+			print(key)
+			authors = get_comment_info(comments[key])
+			for author in authors:
+				if df['owner'][i]["_account_id"] == author:
+					print('author matches')
+				else:
+					file_comment_tuple = (key, author)
+					print(file_comment_tuple)
+					file_comment_tuple_list.append(file_comment_tuple)
+print(len(file_comment_tuple_list))
+
+# for line in df['comments']:
+# 	if isinstance(line,dict):
+# 		for key in line.keys():
+# 			get_comment_info(line[key])
+			

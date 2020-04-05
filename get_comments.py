3e25ee89c56374dd5fca89dbe5b3ed32fbbb25ad
ixgauth <ixgauth@yahoo.com>
2020-04-05 18:49:55 -0400

    This should be the working version of getting the reviewers now

--- get_comments.py
+++ get_comments.py
@@ -70,10 +70,12 @@ import requests
 with open('test_data_with_comments.json') as json_file:
 	data = json.load(json_file)
 
+problem_lines = []
+print(len(data))
 
 i = 0
 for line in data:
-	if i % 200 == 0:
+	if i % 50 == 0:
 		print(i)
 	i+=1
 	change_id = line["change_id"]
@@ -90,16 +92,28 @@ for line in data:
 			owner_id = line['details']['owner']['_account_id']
 
 			for lne in reviewers_list:
-				if lne['_account_id'] == owner_id:
-					continue
-				if lne['_account_id'] == 1022687:
-					continue
+				if '_account_id' in lne.keys() and 'name' in lne.keys():
+					if lne['_account_id'] == owner_id:
+						continue
+					if lne['_account_id'] == 1022687:
+						continue
 
-				account_id_list.append(lne['_account_id'])
-				reviewer_name_list.append(lne['name'])
+					account_id_list.append(lne['_account_id'])
+					reviewer_name_list.append(lne['name'])
+				else:
+					print('problem line', i)
+					problem_lines.append(line)
+
 			line['reviewers_account_id'] = account_id_list
 			line['reviewers_name_list'] = reviewer_name_list
+		else:
+			print('problem line', i)
+			problem_lines.append(line)
 
+print(len(problem_lines))
+for line in problem_lines:
+	data.remove(line)
+print(len(data))
 
 outfile = open("test_data_with_reviewers.json", "w")
 outfile.write(json.dumps(data))	

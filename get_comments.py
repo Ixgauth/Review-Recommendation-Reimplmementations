420e527c3e38cb71e847b0e7c0aa5374a4dbcb27
ixgauth <ixgauth@yahoo.com>
2020-04-05 16:20:00 -0400

    Trying to make the reviewer query work. Stashing these cus I might go another root.

--- get_comments.py
+++ get_comments.py
@@ -6,30 +6,63 @@ from pathlib import Path
 import requests
 
 
-def intake_data_frame(filename):
-	with open(filename) as file:
-		data = json.load(file)
+# def intake_data_frame(filename):
+# 	with open(filename) as file:
+# 		data = json.load(file)
 
-	df = pd.DataFrame(data["commits"])
-	return df
+# 	df = pd.DataFrame(data["commits"])
+# 	return df
 
-# data = []
-# for line in open('test_data.json', 'r'):
-# 	# print(line)
-# 	data.append(json.loads(line))
+# # data = []
+# # for line in open('test_data.json', 'r'):
+# # 	# print(line)
+# # 	data.append(json.loads(line))
 
-# df = pd.DataFrame(data)
+# # df = pd.DataFrame(data)
 
-with open('test_data.json') as json_file:
-	data = json.load(json_file)
+# with open('test_data.json') as json_file:
+# 	data = json.load(json_file)
 
-for line in data:
-	change_id = line["change_id"]
-	baseURL = f"https://gerrit-review.googlesource.com/changes/{change_id}/comments"
+# for line in data:
+# 	change_id = line["change_id"]
+# 	baseURL = f"https://gerrit-review.googlesource.com/changes/{change_id}/comments"
+# 	resp = requests.get(baseURL)
+# 	if(resp.status_code == 200):
+# 		print("it worked")
+# 		line["comments"] = resp.content.decode("utf-8").replace(")]}'",'')
+
+# outfile = open("test_data_with_comments.json", "w")
+# outfile.write(json.dumps(data))
+
+df = pd.read_json('test_data_with_comments.json')
+
+df['details'] = ""
+df['reviewers_account_id'] = ""
+df['reviewers_name_list'] = ""
+
+for i in range(0, len(df)):
+	change_id = df["change_id"][i]
+	baseURL = f"https://gerrit-review.googlesource.com/changes/{change_id}/detail"
 	resp = requests.get(baseURL)
 	if(resp.status_code == 200):
-		print("it worked")
-		line["comments"] = resp.content.decode("utf-8").replace(")]}'",'')
+		df['details'][i] = json.loads(resp.content.decode("utf-8").replace(")]}'",''))
 
-outfile = open("test_data_with_comments.json", "w")
-outfile.write(json.dumps(data))
+		reviewers_qued = df['details'][i]['reviewers']
+		if 'REVIEWER' in reviewers_qued.keys():
+			reviewers_list = reviewers_qued['REVIEWER']
+			account_id_list = []
+			reviewer_name_list = []
+			owner_id = df['details'][i]['owner']['_account_id']
+
+			for lne in reviewers_list:
+				if lne['_account_id'] == owner_id:
+					continue
+				if lne['_account_id'] == 1022687:
+					continue
+
+				account_id_list.append(lne['_account_id'])
+				reviewer_name_list.append(lne['name'])
+			df['reviewers_account_id'][i] = account_id_list
+			df['reviewers_name_list'][i] = reviewer_name_list
+
+df.to_json(r'test_data_with_reviewers.json')
